f6b2181ea8bcbbe8d6c7c2ac485971a44351b67c
Vu Hoang Minh <vuhminh@gmail.com>
2020-03-03 01:19:10 +0700

    ops

--- lib/stela/interfaces/operation.dart
+++ lib/stela/interfaces/operation.dart
@@ -3,7 +3,75 @@ import 'package:inday/stela/interfaces/path.dart';
 import 'package:inday/stela/interfaces/range.dart';
 
 class Operation {
-  static Operation inverse(Operation op) {}
+  static Operation inverse(Operation op) {
+    if (op is InsertNodeOperation) {
+      return RemoveNodeOperation(op.path, op.node);
+    }
+
+    if (op is InsertTextOperation) {
+      return RemoveTextOperation(op.path, op.offset, op.text);
+    }
+
+    if (op is MergeNodeOperation) {
+      return SplitNodeOperation(
+          Path.previous(op.path), op.position, op.target, op.props);
+    }
+
+    if (op is MoveNodeOperation) {
+      Path path = op.path;
+      Path newPath = op.newPath;
+
+      // PERF: in this case the move operation is a no-op anyways.
+      if (Path.equals(newPath, path)) {
+        return op;
+      }
+
+      // We need to get the original path here, but sometimes the `newPath`
+      // is a younger sibling of (or ends before) the original, and this
+      // accounts for it.
+      Path inversePath = Path.transform(path, op);
+      Path inverseNewPath = Path.transform(Path.next(path), op);
+
+      return MoveNodeOperation(inversePath, inverseNewPath);
+    }
+
+    if (op is RemoveNodeOperation) {
+      return InsertNodeOperation(op.path, op.node);
+    }
+
+    if (op is RemoveTextOperation) {
+      return InsertTextOperation(op.path, op.offset, op.text);
+    }
+
+    if (op is SetNodeOperation) {
+      Map<String, dynamic> props = op.props;
+      Map<String, dynamic> newProps = op.newProps;
+
+      return SetNodeOperation(op.path, newProps, props);
+    }
+
+    if (op is SetSelectionOperation) {
+      Range props = op.props;
+      Range newProps = op.newProps;
+
+      if (props == null) {
+        return SetSelectionOperation(newProps, null);
+      }
+
+      if (newProps == null) {
+        return SetSelectionOperation(null, props);
+      }
+
+      return SetSelectionOperation(newProps, props);
+    }
+
+    if (op is SplitNodeOperation) {
+      return MergeNodeOperation(
+          Path.next(op.path), op.position, op.target, op.props);
+    }
+
+    return null;
+  }
 }
 
 class NodeOperation implements Operation {}
