0f279fea0f7f4d0e66046cc54b53cd558ced8909
Vu Hoang Minh <vuhminh@gmail.com>
2020-03-06 22:00:05 +0700

    transforms tests setup

---
+++ test/stela/transforms_test.dart
@@ -0,0 +1,110 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:inday/stela/editor.dart';
+import 'package:inday/stela/element.dart';
+import 'package:inday/stela/node.dart';
+import 'package:inday/stela/operation.dart';
+import 'package:inday/stela/path.dart';
+import 'package:inday/stela/point.dart';
+import 'package:inday/stela/range.dart';
+import 'package:inday/stela/text.dart';
+import 'package:inday/stela/transforms.dart';
+
+class TestEditor extends Editor {
+  TestEditor(
+      {List<Node> children,
+      Range selection,
+      List<Operation> operations,
+      Map<String, dynamic> marks,
+      Map<String, dynamic> props})
+      : super(
+            children: children,
+            selection: selection,
+            operations: operations,
+            marks: marks,
+            props: props);
+
+  @override
+  bool isInline(Element element) {
+    return element is Inline;
+  }
+
+  @override
+  bool isVoid(Element element) {
+    return element is Void;
+  }
+}
+
+void Function(Node, Node) expectEqual = (Node node, Node another) {
+  expect(node.runtimeType, another.runtimeType);
+
+  if (node is Text) {
+    expect(node.text, (another as Text).text);
+    return;
+  }
+
+  if (node is Editor) {
+    expect(
+        RangeUtils.equals(node.selection, (another as Editor).selection), true);
+  }
+
+  for (var i = 0; i < (node as Ancestor).children.length; i++) {
+    Node n = (node as Ancestor).children[i];
+    Node an = (another as Ancestor).children[i];
+
+    expectEqual(n, an);
+  }
+};
+
+void main() {
+  group('delete', () {
+    group('emojis', () {
+      test('inline end reverse', () {
+        // <editor>
+        //   <block>
+        //     <text />
+        //     <inline>
+        //       word📛
+        //       <cursor />
+        //     </inline>
+        //     <text />
+        //   </block>
+        // </editor>
+        TestEditor editor = TestEditor(
+            selection:
+                Range(Point(Path([0, 1, 0]), 6), Point(Path([0, 1, 0]), 6)),
+            children: <Node>[
+              Block(children: <Node>[
+                Text(''),
+                Inline(children: <Node>[Text('word📛')]),
+                Text(''),
+              ])
+            ]);
+
+        Transforms.delete(editor, unit: Unit.character, reverse: true);
+
+        // <editor>
+        //   <block>
+        //     <text />
+        //     <inline>
+        //       word
+        //       <cursor />
+        //     </inline>
+        //     <text />
+        //   </block>
+        // </editor>
+        TestEditor expected = TestEditor(
+            selection:
+                Range(Point(Path([0, 1, 0]), 4), Point(Path([0, 1, 0]), 4)),
+            children: <Node>[
+              Block(children: <Node>[
+                Text(''),
+                Inline(children: <Node>[Text('word')]),
+                Text(''),
+              ])
+            ]);
+
+        expectEqual(editor, expected);
+      });
+    });
+  });
+}
